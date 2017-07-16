affc90fb6a578e6b7e44e35af159da93118bc5d7
Alexandra Ribeiro <aribeiro@student.42.fr>
2017-07-16 20:33:56 +0200

    more Tests + parser

--- avaj_launcher/src/avaj_launcher/Leader.java
+++ avaj_launcher/src/avaj_launcher/Leader.java
@@ -8,14 +8,16 @@ public class Leader {
 	
 	public int nb_cycle;
 	public int nb_aircraft;
+	private String[] sArrayLine;
+	private Parser p;
 	BufferedReader buff = null;
 	
 	public void reader(String[] args) throws IOException, MyExceptions {
-	
+
 		if (args.length == 1) {
 			String sCurrentLine;
 			buff = new BufferedReader(new FileReader(args[0]));
-			Parser p = new Parser();
+			p = new Parser();
 			nb_aircraft = -1;
 
 			while ((sCurrentLine = buff.readLine()) != null) {
@@ -23,23 +25,52 @@ public class Leader {
 
 				if (nb_aircraft == -1) {
 					nb_cycle = p.verifFirstLine(sCurrentLine);
-					System.out.println(nb_cycle); // debug
-					if (nb_cycle == 0)
-						throw new MyExceptions("\n=> Error : not enough cycle, cycle = 0");
-					else if (nb_cycle > 255)
-						throw new MyExceptions("\n=> Error : too much cycle, cycle > 255");
-					else if (nb_cycle < 0)
-						throw new MyExceptions("\n=> Error : error value cycle, cycle < 0");
+					verifCycle();
 				}
-				else
-					p.verifLines(sCurrentLine); //regex lines
+				else {
+					if (p.regex(sCurrentLine) == false)
+						throw new MyExceptions("\n=> Error (regex) : line " + (nb_aircraft + 2)); //error regex
+					else {
+						System.out.println("ok regex"); // debug
+						sArrayLine = sCurrentLine.split(" ");
+						readType();
+					}
+				}
 				nb_aircraft++;
 			}
 		}
 		else {
 			throw new MyExceptions("\n=> Error : arguments, args != 1");
 		}
-		
+
 	}
 	
+	private void verifCycle() throws MyExceptions {
+		if (nb_cycle == 0)
+			throw new MyExceptions("\n=> Error : not enough cycles, cycle = 0");
+		else if (nb_cycle > 255)
+			throw new MyExceptions("\n=> Error : too much cycles, cycle > 255");
+		else if (nb_cycle < 0)
+			throw new MyExceptions("\n=> Error : the value of the cycles is negative");
+	}
+	
+	private void readType() throws MyExceptions {
+		if (p.parserVerifTypes(sArrayLine[0]) == false) {
+			throw new MyExceptions("\n=> Error (parser) : '" + sArrayLine[0] + "' unknown type, line " + (nb_aircraft + 2)); //error Type
+		}
+		else {
+			readID();
+		}
+	}
+	
+	private void readID() throws MyExceptions {
+		System.out.println("ok Type"); //debug
+		if (p.parserVerifID(sArrayLine[1], sArrayLine[0]) == false) {
+			throw new MyExceptions("\n=> Error (parser) : '" + sArrayLine[1] + "' wrong ID, line " + (nb_aircraft + 2)); //error ID
+		}
+		else {
+			System.out.println("ok ID"); //debug
+		}
+	}
+
 }
--- avaj_launcher/src/avaj_launcher/Parser.java
+++ avaj_launcher/src/avaj_launcher/Parser.java
@@ -2,6 +2,8 @@ package avaj_launcher;
 
 public class Parser {
 	
+	public static final String[] sAircrafts = {"Baloon", "Helicopter", "JetPlane" };
+	
 	public int verifFirstLine(String sLine) throws MyExceptions {
 		try {
 			int cycle = Integer.parseInt(sLine);
@@ -12,8 +14,25 @@ public class Parser {
 		}
 	}
 	
-	public void verifLines(String sLine) {
-		
+	public boolean regex(String sLine) {
+		String reg = "^[A-Z][a-zA-Z]+[ ][A-Z][0-9]+[ ][0-9]{1,3}[ ][0-9]{1,3}[ ][0-9]{1,3}$";
+		 return (sLine.matches(reg));
 	}
 	
+	public boolean parserVerifTypes(String sTypes) {
+		int i = 0;
+		while (i < sAircrafts.length)
+		{
+			if (	sTypes.equals(sAircrafts[i]) == true)
+				return true;
+			i++;
+		}
+		return false;
+	}
+	
+	public boolean parserVerifID(String sID, String sTypes) {
+		if (sID.charAt(0) == sTypes.charAt(0))
+			return true;
+		return false;
+	}
 }
