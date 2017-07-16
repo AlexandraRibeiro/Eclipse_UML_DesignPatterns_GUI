d6998afe5d18c0de9f1ec39ef2296328b0fa7f27
Alexandra Ribeiro <aribeiro@student.42.fr>
2017-07-16 22:16:02 +0200

    backup

--- avaj_launcher/src/avaj_launcher/Tools.java
+++ avaj_launcher/src/avaj_launcher/Tools.java
@@ -10,7 +10,7 @@ public class Tools {
     public static final String BLACK = "\u001B[30;1m";
     public static final String RED = "\u001B[31;1m";
     public static final String GREEN = "\u001B[32;1m";
-    public static final String YELLOW = "\u001B[33;1m";
+    public static final String YELLOW = "\u001B[33;1m"; 		//for MyExceptions
     public static final String BLUE = "\u001B[34;1m";
     public static final String PURPLE = "\u001B[35;1m";
     public static final String CYAN = "\u001B[36;1m";
